24442d23c0f5150569436ffe000c90b02fa31b54
GebrialGhirmay <gebrial.ghirmay@lumen-research.com>
2025-06-24 15:07:54 +0100

    Updating main file for new program structure

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-    "go-aws-s3-cli/mycli/cli"
-    "go-aws-s3-cli/mycli/logging" // Imports the logging package
+	"go-aws-s3-cli/cli"
+	"go-aws-s3-cli/logging" // Imports the logging package
 )
 
 func main() {
-    logging.SetLogLevel(0) // Set the log level to debugLevel (0)
-    cli.Execute()
+	logging.SetLogLevel(0) // Set the log level to debugLevel (0)
+	cli.Execute()
 }
